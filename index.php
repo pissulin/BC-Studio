dec348e5abf21c23582e98f937134393efef173e
pissulin <pissulin.ads@gmail.com>
2019-03-04 11:05:27 -0300

    atualização de html para php

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html >
+<html lang="pt-br">
+
+    <head >
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BC-Studio</title>
+        <meta name="description" content="Produtora Musical">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </head>
+    <body lang="pt-br">
+        <main>
+            <header class="header">
+                <div class="headerInterno">
+                    <div class="logo">
+                        <a href="index.html"><img src="/img/Logo_bc_studio_semFundo.png" alt="logo do BC Studio" width="400" ></a>
+                    </div>
+                    <nav class="menu">
+                        <ul>
+                            <li>
+                                <a href="#quem_somos">QUEM SOMOS</a>
+                                <a href="#servicos">SERVIÇOS</a>
+                                <a href="#portfolio">PORTFÓLIO</a>
+                                <a href="#contato">CONTATO</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <section>
+                <article class="intro">
+                    <div class="row">
+                        <div class="texto">
+                            <h2>PRODUÇÃO MUSICAL</h2>
+                            <P>Acompanhar todas as etapas faz toda<br>a diferença no resultado final</P> 
+                        </div>
+                                                                   
+                    </div>
+
+                    <div class="row">
+                        <div class="card">
+                            <img src="/img/pre-producao.jpg" alt="microfone">
+                            <h3>PRÉ-PRODUÇÃO</h3>
+                        </div>
+                        
+                        <div class="card">
+                                <img src="/img/gravacao.jpg" alt="gravação">
+                                <h3>GRAVAÇÃO</h3>
+                        </div>
+                        <div class="card">
+                                <img src="/img/mixagem.jpg" alt="mixagem">
+                                <h3>MIXAGEM</h3>
+                        </div>
+                        <div class="card">
+                                <img src="/img/masterizacao.jpg" alt="masterização">
+                                <h3>MASTERIZAGEM</h3>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="texto border ">
+                            <p>VAMOS CONVERSAR ?</p>
+                            <span class="seta-baixo"></span>
+                        </div>
+                                            
+                </article>
+                <article class="sobre" id="quem_somos">
+                    <h2>QUEM SOMOS</h2>
+                    <div class="texto-esquerdo">
+                            <pre>EXPLORAR NA 
+            <span>SUA ESSÊNCIA,</span>
+                                   O SEU MELHOR.
+                            </pre>
+                            <p>O Estúdio BC  Musica ART e dança esta inovando no mercado de atuação e é uma empresa que atua virtualmente nas divulgações de trabalho de Bruno Cesar, funciona a parti de um escritório em sua casa em mairiporã no Alpes da Cantareira e atua em diversos lugares de são paulo , Hoje atuando em DA2 danças onde participa como DJ no chá da Emy realizado um domingo por mês a mais de 4 anos em alguns lugares específicos de são paulo e hoje em dia a 1 ano na DA2 , ministra aulas de dança de salão na escola Atelie da Ale em santana, av cantareira  de terça , quarta e quinta e faz um baile por mês geralmente no ultimo sábado do mês, e agora em nova parceria aulas no Nucleo de Atendimento Vitale rua  Dr Issaias Salomão , 101 na saude as segundas as 19:30 com dança de salão e 21:00 com sambarock .
+                                    Professor de musica desde os 19 anos de idade, e professor de dança desde os 25 anos de idade o professor Bruno Cesar procura com toda sua experiencia de vida e profissao fugir do sistema academico defasado do Brasil e ensinar seus alunos de maneira mais objetiva, positiva e atualizada possivel .
+                                    Entre em contato conosco exprimente ou faá um orçamento , você sera bem vindo numa aula demostrativa totalmente sem custo.</p>
+                    </div>
+                </article>
+                <article class="servicos" id="servicos">
+                    <div class="row_title">
+                        <h2>SERVIÇOS</h2>
+                        <h3>PRODUÇÃO MUSICAL EM <br>
+                        TODAS AS SUAS ETAPAS                      
+                        </h3>
+                        <p>Acompanhar todas as etapas faz toda<br> a diferença no resultado final.</p>
+                        <span class="paragrafo"><p>Os serviços podem ser contratados individualmente, ou como um pacote completo.</p></span>
+                    </div>
+                    <div class="row_seta_baixo"></div>
+                    <div class="etapas">
+                        <img src="/img/setas.jpg" alt="setas" width="400"  >
+                        <div class="etapa pre">
+                            <div class="etapa_title">
+                                <img src="/img/pre-producao_bot.jpg" alt="pre-produção" width="40px">&nbsp;&nbsp;
+                                <h2>PRÉ-PRODUÇÃO</h2>
+                            </div>
+                            <div class="etapa_texto">
+                                <p>Nesta etapa é realizado o planejamento do projeto: definição de andamento, tom, arranjo das músicas, análise do conteúdo e forma das letras e definição de referências.</p>
+                            </div>
+                        </div>
+                        <div class="etapa gravacao">
+                            <div class="etapa_title">
+                                <img src="/img/gravacao_bot.jpg" alt="gravação" width="40px">&nbsp;&nbsp;
+                                <h2>GRAVAÇÃO</h2>
+                            </div>
+                            <div class="etapa_texto">
+                                <p>Nesta etapa são captadas as performances individuais de cada instrumento que compõe a faixa.</p>
+                            </div>
+                        </div>
+                        <div class="etapa mixagem">
+                            <div class="etapa_title">
+                                <img src="/img/mixagem_bot.jpg" alt="mixagem" width="40px">&nbsp;&nbsp;
+                                <h2>MIXAGEM</h2>
+                                </div>
+                                <div class="etapa_texto">
+                                    <p>Processo onde o conhecimento técnico das ferramentas de manipulação de áudio é colocado em prática para juntar todos os instrumentos de forma artística e criativa.</p>
+                                </div>
+                        </div>
+                        <div class="etapa masterizacao">
+                                <div class="etapa_title">
+                                    <img src="/img/masterizacao_bot.jpg" alt="masterização" width="40px">&nbsp;&nbsp;
+                                    <h2>MASTERIZAÇÃO</h2>
+                                    </div>
+                                    <div class="etapa_texto">
+                                        <p>Estágio final na produção, onde são feitos os últimos retoques na música e as adaptações técnicas adequadas à mídia de veiculação do produto.</p>
+                                    </div>
+                            </div>
+                    </div>
+                </article>
+
+                <article class="portfolio" id="portfolio">
+                    <div class="row-portfolio-titulo">
+                        <h2>PORTFÓLIO</h2>
+                        <div class="row_seta_baixo"></div>
+                    </div>
+
+                    <div class="cards">
+                        <div class="card">item-1</div>
+                        <div class="card">item-2</div>
+                        <div class="card">item-3</div>
+                        <div class="card">item-4</div>
+                        <div class="card">item-5</div>
+                        <div class="card">item-6</div>
+                        <div class="card">item-7</div>
+                    </div>
+                </article>
+                <article class="contato" id="contato">
+                    
+                    <form action="contato.php" method="POST">
+                            
+                        <div class="inputs">
+                        <input type="text" name="nome" id="nome" placeholder="Nome" required>
+                        <input type="email" name="email" id="email" placeholder="Email" required>
+                        <input type="text" name="endereco" id="endereco" placeholder="Endereço" >
+                        <input type="tel" name="telefone" id="telefone" placeholder="Telefone" required>
+                        </div>
+                        <div class="texto_submit">
+                        <textarea name="mensagem" id="mensagem" cols="30" rows="4" placeholder="Mensagem" required></textarea><br>
+                        <input type="submit" value="Enviar">
+                        </div>
+                    </form>
+                    <div class="social">
+                        <ul>
+                            <li>
+                                <a href="https://soundcloud.com/" target="__blank"><img src="/img/nuvem.png" alt=""></a>
+                                <a href="https://www.facebook.com/" target="__blank"><img src="/img/facebook.png" alt=""></a>
+                                <a href="https://www.instagram.com/" target="__blank"><img src="/img/instagram.png" alt=""></a>
+                            </li>
+                        </ul>
+                    </div>
+                </article>
+             
+            </section>
+            <footer>
+                <div class="container">
+                        <div class="logo-inferior">
+                            <a href="index.html" ><img src="/img/Logo_bc_studio_semFundo.png" width="130"   alt=""></a>
+                        </div>
+                        <div class="inf-contatos">
+                            <p class="telefone"><a href="tel:11972781535 ">(11) 97278-1535 </a> | <a href="tel:114551935278">(11) 584193-5278</a><br>
+                            <a href="mailto:bcstudioeproducoes@gmail.com">bcstudioeproducoes@gmail.com</a></p><br>
+                            <address> <a href="https://goo.gl/maps/Xt6GXkZ7qAR2" target="_blank"> <p>Rua da coica, 80<br>
+                            Vila Alpina | Mairiporã | SP</p></a></address>
+                        </div>
+                        <div class="desenvolvedor">
+                            <p class="direitos">Todos os direitos reservados | Criado por:<a href="https://pissulin.github.io/pissulin/" target="_blank">Dev Pissulin</a></p>
+                        </div>
+                        <a class="whats" href="https://bit.ly/2Tpf1PG" target="_blank"><img src="/img/whatsapp-brands.svg" width="50" alt=""><br> Abrir whatsApp</a>
+                </div>
+            </footer>
+            
+        </main>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
